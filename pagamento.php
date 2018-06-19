ee9a6354a8c38a098ea209afb34260a64bd61641
hav00x <luuiz243@hotmail.com>
2018-06-19 15:53:02 -0300

    18/06

---
+++ pagamento.php
@@ -0,0 +1,157 @@
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>root. | abcde</title>
+  <link rel="icon" href="imagens/favicon.png">
+
+  <!-- Bootstrap -->
+  <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="css/estilo.css?=ver31232133335454 " rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.min.css">
+
+  <!-- IE 9 ou menor -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+  </head>
+  <body>
+
+    <?php
+    include('templates/sidebar.php');
+    ?>
+
+    <!-- Page Content Holder -->
+    <div id="content">
+      <div class="section">
+        <div class="row">
+          <div class="col-md-6">
+            <img style="width: 25px; margin: 5px;" src="img/notification.png">
+          </div>
+          <div class="col-md-6">
+            <button type="button" style="float: right;" class="btn btn-info navbar-btn">
+              <span class="glyphicon glyphicon-log-out"></span>
+              <span>Sair</span>
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div class="section">
+        <div class="row">
+          <div class="col-md-12">
+            <ul class="nav nav-tabs edit-tabs">
+              <li role="presentation" class="active tab-descri"><a href="#andamento" aria-controls="andamento" role="tab" data-toggle="tab">A Receber</a></li>
+              <li role="presentation"><a href="#concluido" class="tab-descri" aria-controls="concluido" role="tab" data-toggle="tab">A Efetuar</a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-12">
+            <div class="tab-content">
+
+              <div role="tabpanel" class="tab-pane active" id="andamento">
+
+                <div class="col-md-12">
+                  <h2>PROJETOS</h2>
+                </div>
+
+                <div class="col-md-4">
+                  <div id="div-projetos">
+                    <h4>Titulo:</h4>
+                    <p>Descrição:</p>
+                    <p>Cliente:</p>
+                    <p>Valor:</p>
+                    <p>Entregue:</p>
+                    <button class="btn btn-details">Ver detalhes</button>
+                  </div>
+                </div>
+              </div>
+              
+
+              <div role="tabpanel" class="tab-pane" id="concluido">
+
+              </div>
+
+
+            </div>
+
+          </div>
+
+          <button type="button" style="float: right;" class="button -regular backcolr" data-target="#modalCadastro" data-toggle="modal" href="#modalCadastro">+ Novo Projeto</button>
+
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- Modal -->
+  <div class="modal fade" id="modalCadastro" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+          <h3 class="modal-title">Novo Projeto</h3>
+        </div>
+        <div class="modal-body">
+          <form>
+            <div class="col-left">
+              <label for="nomecli">Nome do Cliente/Empresa</label>
+              <input type="text" class="text_field" id="nomecli" name="">
+            </div>
+            <div class="col-right">
+              <label for="tipopro">Tipo de Projeto</label>
+              <input type="text" class="text_field" id="tipopro" name="">
+            </div>
+            <div class="col-cent">
+              <label for="descri">Descrição do Projeto</label>
+              <textarea id="descri" maxlength="254" class="text_field"></textarea>
+            </div>
+            <div class="col-left">
+              <label for="dataini">Data Início</label>
+              <input type="text" class="text_field" id="danaini" name="">
+            </div>
+            <div class="col-right">
+              <label for="dataent">Data Entrega</label>
+              <input type="text" class="text_field" id="dataent" name="">
+            </div>
+          </form>
+        </div>
+        <div class="modal-footer" style="clear: both;">
+          <button type="button" class="button -regular" data-dismiss="modal">Voltar</button>
+          <button type="button" class="button -regular">Criar</button>
+        </div>
+      </div>
+    </div>
+  </div><!-- Fim Modal -->
+
+  <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
+
+  <script src="bootstrap/js/bootstrap.min.js"></script>
+  <!-- jQuery Custom Scroller CDN -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.concat.min.js"></script>
+
+  <script type="text/javascript">
+    $(document).ready(function () {
+      $("#sidebar").mCustomScrollbar({
+        theme: "minimal"
+      });
+
+      $('#sidebarCollapse').on('click', function () {
+        $('#sidebar, #content, #btnMenu').toggleClass('active2');
+        $('.collapse.in').toggleClass('in');
+        $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+      });
+    });
+  </script>
+
+
+
+</body>
+</html>
