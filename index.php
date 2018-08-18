9da117dc9b97851d07b596ead2601be63bab4f2a
hav00x <luuiz243@hotmail.com>
2018-08-18 12:21:38 -0300

    tirando os bugs

--- index.php
+++ index.php
@@ -35,23 +35,13 @@ $erro_login = isset($_GET['errologin']) ? $_GET['errologin'] : 0;
       }
     });
   </script>
-<<<<<<< HEAD
-
-</head>
-=======
-</head>
-
->>>>>>> f9afc34b3faf7793dfb529ef2f5f617fe9ba8844
   <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-<<<<<<< HEAD
-=======
   </head>
->>>>>>> f9afc34b3faf7793dfb529ef2f5f617fe9ba8844
 
   <body class="indexback">
     <?php
